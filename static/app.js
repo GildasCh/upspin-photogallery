bb2b5d993636e4baf90fc300917baac035f03cc7
Gildas Chabot <gildaschbt@gmail.com>
2017-12-26 18:49:26 +0100

    html: add next & previous bound to left & right

--- static/app.js
+++ static/app.js
@@ -1,13 +1,36 @@
 var app = new Vue({
     el: '#app',
     data: {
+        imageCollection: files,
         theaterImage: '',
-        showTheaterImage: false
+        showTheaterImage: false,
+        number: 0,
+        event: {}
+    },
+    created: function () {
+        window.addEventListener('keyup', this.key);
     },
     methods: {
-        show: function (imgPath) {
-            this.theaterImage = imgPath;
+        show: function (imgNumber) {
+            this.number = imgNumber;
+            this.theaterImage = '/f/' + this.imageCollection[this.number];
             this.showTheaterImage = true;
+        },
+        key: function(keyEvent) {
+            if (keyEvent.key == "ArrowLeft") {
+                this.previous();
+            } else if (keyEvent.key == "ArrowRight") {
+                this.next();
+            }
+        },
+        next: function () {
+            this.number = (this.number + 1) % this.imageCollection.length;
+            this.theaterImage = '/f/' + this.imageCollection[this.number];
+        },
+        previous: function () {
+            this.number = (this.number - 1 + this.imageCollection.length) %
+                this.imageCollection.length;
+            this.theaterImage = '/f/' + this.imageCollection[this.number];
         },
         hide: function () {
             this.showTheaterImage = false;
