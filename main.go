c8f8a14640842a3d15c6447ec192a2e8419710d0
Gildas Chabot <gildaschbt@gmail.com>
2017-12-26 12:58:54 +0100

    Serve the filenames as a JSON at /api/*
    We use the Gin HTTP Framework

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/gin-gonic/gin"
 	"upspin.io/client"
 	"upspin.io/config"
 	_ "upspin.io/transports"
@@ -17,12 +19,29 @@ func main() {
 	}
 
 	client := client.New(cfg)
-	entries, err := client.Glob("augie@upspin.io/Images/Augie/***")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, entry := range entries {
-		fmt.Println(entry.Name)
-	}
+
+	router := gin.Default()
+
+	router.GET("/api/*path", func(c *gin.Context) {
+		path := strings.TrimPrefix(c.Param("path")+"*", "/")
+
+		entries, err := client.Glob(path)
+		if err != nil {
+			fmt.Printf("Could not list pattern %q, err: %v\n",
+				path, err)
+			c.Status(500)
+			return
+		}
+
+		filenames := []string{}
+		for _, entry := range entries {
+			filenames = append(filenames, string(entry.Name))
+		}
+
+		c.JSON(200, gin.H{
+			"files": filenames,
+		})
+	})
+
+	router.Run()
 }
